b8bd761a679ede331dbe8f786bfdd5700ae530f9
noeyix <96297187+noeyix@users.noreply.github.com>
2022-01-22 23:52:04 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -21,22 +21,30 @@ include("navbar.php");
 
 <style>
 
+
+body{
+    background-color: #FFBF66;
+}
 *{
   box-sizing: border-box;
 }
 
 .productimage{
     height: 300px;
-    width: 292.5px;
+    width: 289.8px;
 }
 
 .card1{
     transition: 0.5s;
-    height: 500px;
+    height: 600px;
     width: 292.5px;
+    border: 1px;
+    border-color: #FFBF66;
+    border-style: solid;
+    background-color: #FFFFFF;
 }
 .card1:hover{
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 8px 16px 0 #FFBF66;
 }
 .container1{
     padding: 2px 16px;
@@ -51,8 +59,170 @@ include("navbar.php");
                 font-size: 20px;
                 border-radius: 2px;
 }
+.items{
+    float: left;
+    padding-left: 110px;
+    padding-top: 50px;
+}
+
+.slideshow {
+    float: right;
+    width: 500px;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top:20px;
+    padding-bottom: 20px;
+    background-color: #FFBF66;
+    padding-top: 50px;
+}
+
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+
+.loginbutton{
+    background-color: #FFBF66;
+    color: #000000;
+}
 </style>
 <body>
+
+<div class="slideshow">
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 4</div>
+  <img src="images/strongpweng.jpg" style="width:100%; border: solid;">
+  <div class="text">skidiki</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 4</div>
+  <img src="images/phisheng.jpg" style="width:100%; border: solid;">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 4</div>
+  <img src="images/antieng.jpg" style="width:100%; border: solid;">
+  <div class="text">Caption Three</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">4 / 4</div>
+  <img src="images/updateeng.jpg" style="width:100%; border: solid;">
+  <div class="text">Caption Three</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+</div>
+
+<div class="items">
 <?php
 
     $ref_table = "product";
@@ -73,8 +243,8 @@ include("navbar.php");
                         <tr style="font-size: 20px">
                         <td><img class="productimage" src="<?=$row['Image'];?>"><img></td>
                         <div class="container1">
-                            <td><p style="text-align:center;"><?=$row['Product_Name'];?></p></td>
-                        <td><p style="text-align:center;"><?=$row['Price'];?></p></td>
+                            <td><p style="text-align:center; font-size: 30px;"><?=$row['Product_Name'];?></p></td>
+                        <td><p style="text-align:center; font-size: 20px;"><?=$row['Price'];?></p></td>
                         </div>
                         <br><br>
                         <div style="text-align: center;">
@@ -95,13 +265,44 @@ include("navbar.php");
                 <?php
             }
 ?>
-<div>
-<p style="color: white;">.</p>
 </div>
-</div>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+
+
 
 </body>
 </html>
+<footer>
 <?php
 include('footer.php');
 ?>
+</footer>
--- navbar.php
+++ navbar.php
@@ -15,8 +15,9 @@
             ul {
                 list-style-type: none;
                 margin: 0;
-                padding: 0;
                 overflow: hidden;
+                background-color: #FDDBAC;
+                width: 100%;
             }
 
             li {
@@ -29,11 +30,12 @@
                 text-align: center;
                 padding: 14px 16px;
                 text-decoration: none;
+                background-color: #FDDBAC;
 
             }
 
             li a:hover {
-                background-color: #FFE200;
+                background-color: #FFFFFF;
             }
 
             .navbaredit {
@@ -48,16 +50,23 @@
             }
 
             .navbaredit:hover {
-                background-color: #FFE200;
+                background-color: #FFFFFF;
             }
 
         </style>
 
+    <div style="background-color: #EBA442; padding-left: 50px; fontsize: 80px; border-bottom-style: solid; text-align:center; letter-spacing: 9px;">
+        <b><a style=" font-size: 40px;">MPG34SECUREGROCERIES</a></b>
+        </div>
 
-        <div style="width: 100%; background-color:#F0F0F0">
-            <div style="width: 1170px; margin:auto; background-color:#F0F0F0">
+
+        <div style="width: 100%; background-color:#FDDBAC">
+        
+            <div style="width: 100%; margin:auto; background-color:#FDDBAC; border-bottom-style: solid; padding-right:40px;">
+            
 
                 <ul>
+                    
                     <a class="w3-right navbaredit" href="login.php">MY ACCOUNT</a>
                     <?php
                 if(isset($_SESSION['user_id']))
@@ -71,14 +80,16 @@
                     
                 }
                 ?>
+                <a class="w3-left navbaredit" href="index.php">ENGLISH</a>
                 <a class="w3-left navbaredit" href="indexchi.php">CHINESE</a>
                 <a class="w3-left navbaredit" href="login.php">MALAY</a>
                 <a class="w3-left navbaredit" href="login.php">TAMIL</a>
+                <a data-fancybox="gallery" class="w3-left navbaredit" href="images/1.png">HOW TO USE</a>
+            
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.0/jquery.fancybox.min.css" rel="stylesheet" />
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.0/jquery.fancybox.min.js"></script>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.0/jquery.fancybox.min.css">
-                <a data-fancybox="gallery" class="w3-left navbaredit" href="images/1.png">HOW TO USE</a>
                 </ul>
 
             </div>
@@ -87,12 +98,14 @@
         </div>
 
         <div style="width: 100%;">
-            <div style="width: 1170px; margin:auto;">
+            <div style="width:100%; margin:auto; float:right; font-size: 20px; border-bottom-style: solid; background-color: #FDDBAC;">
+            
                 <header class="w3-container w3-xlarge">
-                    <p class="w3-left"><a href="index.php" style="text-decoration: none;">MPG34SECUREGROCERIES</a></p>
                     <p class="w3-right">
-                    <a href="search.php" class="fa fa-search" style="text-decoration: none;">SEARCH</a>
-                    <a href="cart.php" class="fa fa-shopping-cart w3-margin-right" style="text-decoration: none;">CART</a>
+                        
+                    <a href="search.php" class="fa fa-search" style="text-decoration: none; padding-right:20px;"> SEARCH </a>
+                    <a style="padding-right:40px;"> </a>
+                    <a href="cart.php" class="fa fa-shopping-cart w3-margin-right" style="text-decoration: none;"> CART </a>
                     </p>
                 </header>
             </div>
@@ -101,10 +114,9 @@
         </div>
 
         <div style="width: 100%; border-top: 1px solid #F0F0F0; border-bottom: 1px solid #F0F0F0;">
-            <div style="width: 1170px; margin:auto;">
+            <div style="margin:auto;">
 
                 <ul>
-                    <li><a href="index.php" style="font-size: 20px;">STORE</a></li>
                     <?php
                          if(isset($_SESSION['verified_admin']))
                          {
