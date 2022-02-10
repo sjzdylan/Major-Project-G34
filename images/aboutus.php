34718cda16a96ba3b0534c349272f30b4cb4300e
noeyix <96297187+noeyix@users.noreply.github.com>
2022-02-10 21:31:23 +0800

    Add files via upload

---
+++ images/aboutus.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Webpage Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        
+        <style>
+
+            body{
+                background: #FFBF66;
+
+
+            }
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                color: black;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+
+
+            }
+
+            li a:hover {
+                background-color: #F0F0F0;
+            }
+
+            .navbaredit {
+                display: block;
+                color: #9b9b9b;
+                text-align: center;
+                padding: 8px 12px;
+                text-decoration: none;
+                font-size: 12px;
+
+
+            }
+
+            .navbaredit:hover {
+                color: black;
+            }
+
+            .loginfields {
+                border: 1px solid #F0F0F0;
+                width: 493px;
+                height: 34px;
+                font-size: 14px;
+                padding-left: 10px;
+                color: #9b9b9b;
+            }
+            
+            .loginbutton {
+                background-color: black;
+                color: white;
+                border: none;
+                cursor: pointer;
+                padding: 4px 10px;
+                font-size: 14px;
+                border-radius: 2px;
+            }
+            
+
+        </style>
+
+<?php
+include('navbar.php');
+?>
+   
+    <br>
+    <br>
+    <br>
+    <div class="w3-container" style="width: 1270px; margin: auto;">
+    <div style="width: 585px; float:left; overflow:hidden;">
+    <b style="font-size: 25px;">INTRODUCTION</b>
+    <p style="font-size: 20px;">As the digital age starts to become more apparent in the lives of people, there is an increasing trend in usage of devices such as computers and mobile phones. This leads to many elderly to be left out as some may not be able to comprehend or understand how such devices work, while the others do not seem to care at all. Hence, as elderly slowly integrate into the digital age, there are cases where the elderly fall for scams and cyberattacks due to them not having the knowledge of it and how to prevent it. Hence, there is a need to inform and keep them updated on the types of scams and cyberattack that are prevalent so as to prevent them from falling into it.</p>
+    <br>
+    <b style="font-size: 25px;">OBJECTIVES</b>
+    <p style="font-size: 20px;">The objectives meant to be achieved through this project are to develop a secure platform for the elderly to integrate in the cyber world, to ensure that the platform is user friendly for them, and to create awareness of cyber-attacks such as online scams. </p>
+    <br>
+    <b style="font-size: 25px;">SCOPE</b>
+    <p style="font-size: 20px;">The scope of this project is to build a secured and elderly-friendly web application for senior citizens. The project is planned to be completed within the assigned time frame of 10 weeks. This website will allow the elderly to experience seamless yet secured transactions while raising awareness regarding the different types of scams at the same time.</p>
+    <br>
+    </div>
+
+    </div>
+
+    <br>
+    <br>
+<?php
+include('footer.php');
+?>
+    </body>
+</html>
